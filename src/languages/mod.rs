110c6b9fc18bc3a9307fbbd260cea5523f49f73c
aoife cassidy <aoife@enby.space>
2024-07-07 11:54:02 -0700

    languages: support node

--- src/languages/mod.rs
+++ src/languages/mod.rs
@@ -8,7 +8,7 @@ mod node;
 pub fn run_language(language: String, version: String, location: path::PathBuf) -> Result<()> {
     match language.as_str() {
         "cargo" | "rust" => cargo::bump(version, location)?,
-        "node" | "javascript" | "js" | "typescript" | "ts" => node::bump(version)?,
+        "node" | "javascript" | "js" | "typescript" | "ts" => node::bump(version, location)?,
         unknown => bail!("unsupported language {unknown}. see `nanpa list-languages` for a list of supported languages"),
     }
 
--- src/languages/node.rs
+++ src/languages/node.rs
@@ -1,5 +1,22 @@
-use anyhow::{bail, Result};
+use anyhow::Result;
+use serde_json::Value;
+use std::{fs, io::Write, path};
 
-pub fn bump(version: String) -> Result<()> {
-    todo!();
+pub fn bump(version: String, location: path::PathBuf) -> Result<()> {
+    let json = fs::read_to_string(location.join("package.json"))?;
+    let mut doc: Value = serde_json::from_str(json.as_str())?;
+
+    if let Some(v) = doc.get_mut("version") {
+        *v = Value::from(version);
+    }
+
+    let mut f = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(location.join("package.json"))?;
+    f.write_all(serde_json::to_string_pretty(&doc)?.as_bytes())?;
+    f.flush()?;
+
+    Ok(())
 }
