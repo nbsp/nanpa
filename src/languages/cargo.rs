a3203ba4792d95b33b6b90698462d7b12cdad5f9
aoife cassidy <aoife@enby.space>
2024-12-03 15:19:38 +0200

    fix: cargo roots

--- src/languages/cargo.rs
+++ src/languages/cargo.rs
@@ -21,32 +21,28 @@ pub fn bump(version: String, location: path::PathBuf) -> Result<()> {
     // update dependent packages' manifests
     if let Some(path) = location.parent() {
         env::set_current_dir(path)?;
-        if let Some(root) = find_root(false) {
-            let root = fs::canonicalize(root).unwrap();
-            env::set_current_dir(root)?;
-            for file in glob("**/.nanparc")? {
-                let file = file?;
-                let mut needs_change = false;
-                if let Ok(toml) =
-                    fs::read_to_string(file.parent().unwrap().join("Cargo.toml").clone())
-                {
-                    let mut doc = toml.parse::<DocumentMut>()?;
-                    if let Some(deps) = doc.get("dependencies") {
+        goto_highest_root()?;
+        for file in glob("**/.nanparc")? {
+            let file = file?;
+            let mut needs_change = false;
+            if let Ok(toml) = fs::read_to_string(file.parent().unwrap().join("Cargo.toml").clone())
+            {
+                let mut doc = toml.parse::<DocumentMut>()?;
+                if let Some(deps) = doc.get("dependencies") {
+                    if let Some(dep) = deps.get(name) {
+                        if let Some(_) = dep.get("version") {
+                            needs_change = true;
+                            doc["dependencies"][name]["version"] = value(version.clone())
+                        }
+                    }
+                }
+                if let Some(workspace) = doc.get("workspace") {
+                    if let Some(deps) = workspace.get("dependencies") {
                         if let Some(dep) = deps.get(name) {
                             if let Some(_) = dep.get("version") {
                                 needs_change = true;
-                                doc["dependencies"][name]["version"] = value(version.clone())
-                            }
-                        }
-                    }
-                    if let Some(workspace) = doc.get("workspace") {
-                        if let Some(deps) = workspace.get("dependencies") {
-                            if let Some(dep) = deps.get(name) {
-                                if let Some(_) = dep.get("version") {
-                                    needs_change = true;
-                                    doc["workspace"]["dependencies"][name]["version"] =
-                                        value(version.clone())
-                                }
+                                doc["workspace"]["dependencies"][name]["version"] =
+                                    value(version.clone())
                             }
                         }
                     }
@@ -64,5 +60,19 @@ pub fn bump(version: String, location: path::PathBuf) -> Result<()> {
         }
     }
 
+    Ok(())
+}
+
+fn goto_highest_root() -> Result<()> {
+    let cwd = env::current_dir()?;
+    if let Some(parent) = env::current_dir()?.parent() {
+        env::set_current_dir(parent)?;
+    }
+    if let Some(root) = find_root(false) {
+        let root = fs::canonicalize(root).unwrap();
+        env::set_current_dir(root)?;
+        return goto_highest_root();
+    }
+    env::set_current_dir(cwd)?;
     Ok(())
 }
